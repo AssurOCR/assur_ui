fc9facc39eeea37d90df688c8780f9fd6d0698b9
Rudrabhoj Bhati <hello@rudrabhoj.com>
2023-01-27 01:57:00 +0530

    Simplifying Rust arguments to argc and argv translation

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,24 @@ extern "C" fn print_hello() {
     println!("Hello, World! from Rust");
 }
 
-fn main() {
-    // Get the command line arguments for argc and argv
+fn get_argc_argv() -> (i32, *const *const i8) {
     let args: Vec<String> = std::env::args().collect();
     let argc = args.len() as i32;
 
-    let mut argv: Vec<*const c_char> = Vec::with_capacity(argc as usize);
+    let mut argv: Vec<*const i8> = Vec::with_capacity(argc as usize);
     for arg in args {
         argv.push(CString::new(arg).unwrap().into_raw());
     }
 
+    (argc, argv.as_ptr())
+}
+
+fn main() {
+    let (argc, argv) = get_argc_argv();
+
     // Pass the command line arguments to start_wx_system
     unsafe {
         set_func_pointer(print_hello);
-        start_wx_system(argc, argv.as_ptr());
+        start_wx_system(argc, argv);
     }
 }
